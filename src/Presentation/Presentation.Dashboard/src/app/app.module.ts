dd376b46cffadeb7b556af40a1f2190c560b5c58
AmirH6977 <107415904+AmirH6977@users.noreply.github.com>
2023-01-12 12:35:58 +0330

    templateAdd

--- src/Presentation/Presentation.Dashboard/src/app/app.module.ts
+++ src/Presentation/Presentation.Dashboard/src/app/app.module.ts
@@ -1,33 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderCoponent } from './header/header.component';
-import { ClipBoardComponent } from './clipBoard/clipBoard.component';
-import { ClipBoardListComponent } from './clipBoard/clipBoard-list/clipBoard-list.component';
-import { ClipBoardItemComponent } from './clipBoard/clipBoard-list/clipBoard-item/clipBoard-item.component';
-import { ColorUsedService } from './help/color-used.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderCoponent,
-    ClipBoardComponent,
-    ClipBoardListComponent,
-    ClipBoardItemComponent,
-
+    SignupComponent,
+    LandingComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    FormsModule
-
+    HomeModule
   ],
-  providers: [ColorUsedService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
